447798d362335e938a747c04fd85cfbfcedcad25
Nastaran Motiee <nnastaran83@gmail.com>
2025-03-01 18:39:20 +0200

    Enhance tab layout with custom header and tab bar styles for improved UI

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,19 @@
 import { Tabs } from 'expo-router';
-import IonBarIcon from '@expo/vector-icons/Ionicons';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={ {
-        tabBarActiveTintColor: '#ffd33d'
+        tabBarActiveTintColor: '#ffd33d',
+        headerStyle: {
+          backgroundColor: '#25292e',
+        },
+        headerShadowVisible: false,
+        headerTintColor: '#fff',
+        tabBarStyle: {
+          backgroundColor: '#25292e',
+        },
       } }>
       <Tabs.Screen name="index" options={ {
         title: 'Home',
